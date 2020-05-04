ff43bf362a841643f61ef187905fd3cd03a9ebce
LEBANSAIS <c.lebansais@icloud.com>
2020-05-04 21:22:54 +0200

    Update onewire.php

--- desktop/php/onewire.php
+++ desktop/php/onewire.php
@@ -52,10 +52,7 @@ $deamonRunning = onewire::deamon_info();
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
          <legend><i class="fas fa-cog"></i>{{Gestion}}</legend>
 		<div class="eqLogicThumbnailContainer">
-        <!-- test -->
-<!--        <ul class="nav nav-tabs" role="tablist">
-		</ul> -->	
-        <!-- fin test -->
+
 		    <div class="cursor" id="bt_owfsTable" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 200px;margin-left : 10px;" >
 			    <center>
 				    <i class="fas fa-project-diagram" style="font-size : 5em;color:indigo;"></i>
@@ -121,6 +118,7 @@ $deamonRunning = onewire::deamon_info();
                 
 <!-- configuration des équipement -->
     <!-- Liste des onglets -->
+    <div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
     <ul class="nav nav-tabs" role="tablist">
         <!-- Bouton de retour -->
         <li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab"
