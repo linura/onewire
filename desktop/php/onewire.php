617820f31a177bfd9c2f3ec8b9f27086b1404f5e
Linura <13700448+linura@users.noreply.github.com>
2020-05-07 16:28:59 +0200

    Update onewire.php

--- desktop/php/onewire.php
+++ desktop/php/onewire.php
@@ -69,9 +69,7 @@ if ($deamonRunning['state'] != 'ok') {
                 <center>
                     <i class="fas fa-plus-circle" style="font-size : 7em;color:green;"></i>
                 </center>
-                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#BE0104">
-                    <center>Ajouter</center>
-                </span>
+                <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
             </div>
             <?php
             foreach ($eqLogics as $eqLogic) {
