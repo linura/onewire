f0cb5cb6e9d1459df6b9599e8ed93c2a9341c769
Linura <13700448+linura@users.noreply.github.com>
2020-05-12 11:40:19 +0200

    Update onewire.ajax.php

--- core/ajax/onewire.ajax.php
+++ core/ajax/onewire.ajax.php
@@ -85,7 +85,7 @@ try {
         $sensor_class =  init('sensor_class');
         $cmd = cmd::byId(init('sensor_id'));
         //return $cmd->getValue(true);
-        return onewirecmd::getValue($cmd,$sensor_class);
+        return "good";
     }
     if (init('action') == 'AddSendHistory') {
         $sensor_class =  init('sensor_class');
