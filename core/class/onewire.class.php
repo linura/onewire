fb6f3a2f1a0c9462d1f3c9b4e75bc9a4c589320f
LEBANSAIS <c.lebansais@icloud.com>
2020-10-12 21:36:17 +0200

    ok

--- core/class/onewire.class.php
+++ core/class/onewire.class.php
@@ -653,11 +653,11 @@ class onewireCmd extends cmd
 					$this->save();
 				}
 				$loop_sec_read = 0;
-				while ($loop_sec_read < 3) {	//boucle de seconde lecture en cas d'erreur sur lors de la premiere lecture pour confirmer l'erreur
+				while ($loop_sec_read < 2) {	//boucle de seconde lecture en cas d'erreur sur lors de la premiere lecture pour confirmer l'erreur
 
 					$temp = $this->getValue(false);
 
-					if ((int) $temp == 85 && $loop_sec_read == 2) {
+					if ((int) $temp == 85 && $loop_sec_read == 1) {
 						log::add('onewire', 'debug', 'La sonde est en erreur on ne fait rien. Merci de verifier le composant ou le cablage');
 						message::add('onewire', 'La sonde ' . $equipement->getName() . ' est en erreur. Merci de verifier le composant ou le cablage. Valeur lue: ' . $temp);
 						return false;
